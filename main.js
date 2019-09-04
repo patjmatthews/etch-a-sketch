fc5cf074464d5d2a7f8404029f0c2ae34a8cf749
Patrick_Matthews <patrickjmatthews@gmail.com>
2019-09-04 14:38:15 -0700

    First push from work

---
+++ main.js
@@ -0,0 +1,11 @@
+
+for (let i = 5; i <= 16; i++) {
+    let div = document.createElement("div");
+    
+    div.classList.add("container");
+    console.log(div);
+    
+    
+}
+
+document.body.appendChild(div);
